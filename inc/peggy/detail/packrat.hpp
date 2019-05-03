db7b1995c4b99fa6e52b191f6a09f7d5e839965d
Danilo Margarido <bit2shift@gmail.com>
2019-05-03 02:51:17 +0100

    Some upgrades

--- inc/peggy/detail/packrat.hpp
+++ inc/peggy/detail/packrat.hpp
@@ -1,10 +1,20 @@
 #pragma once
 
+#include <experimental/type_traits>
 #include <istream>
 #include <type_traits>
 
 namespace peggy::detail
 {
+	template<typename... S>
+	using apply_t = void(std::istream&, std::streamoff, std::streamoff, S&&...) noexcept;
+
+	template<typename A>
+	using apply_op = decltype(A::apply);
+
+	template<typename A, typename... S>
+	constexpr bool action_has_apply = std::experimental::is_detected_exact_v<apply_t<S...>, apply_op, A>;
+
 	template<typename R>
 	struct packrat
 	{
@@ -18,7 +28,11 @@ namespace peggy::detail
 			if(std::streamoff offset = input.tellg(); offset == begin)
 				input.seekg(end);
 			else if(R::template do_match<action>(input, state...))
-				action<rule>::apply(input, begin = offset, end = input.tellg(), state...);
+			{
+				begin = offset, end = input.tellg();
+				if constexpr(action_has_apply<action<rule>, S...>)
+					action<rule>::apply(input, begin, end, state...);
+			}
 			else
 			{
 				input.seekg(offset);
